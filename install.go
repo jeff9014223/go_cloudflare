34643a0add02deadcf54315e844a41a6c9090f2c
jeff9014223 <9014223@student.zadkine.nl>
2024-03-10 02:06:22 +0100

    Fixed os.MkdirAll

--- install.go
+++ install.go
@@ -5,13 +5,10 @@ import (
 )
 
 func installTunnel(executable *[]byte) (string, error) {
-	err := os.MkdirAll("/var/tmp/tunnel", 0755)
-	if err != nil {
-		return "", err
-	}
+	os.MkdirAll("/var/tmp", 0755)
 
 	executablePath := "/var/tmp/tunnel"
-	err = os.WriteFile(executablePath, *executable, 0755)
+	err := os.WriteFile(executablePath, *executable, 0755)
 	if err != nil {
 		return "", err
 	}
